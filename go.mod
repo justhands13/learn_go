ad430e02989a5490f19e6bf5559c6e57b06b02ff
Shashank Pant <shashank11235@gmail.com>
2023-06-05 23:11:15 +0530

    using other packages

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/justhands13/learn_go
 
 go 1.20
+
+replace github.com/justhands13/reverse_int v0.0.0 => ../reverse_int
+
+require github.com/justhands13/reverse_int v0.0.0
+
+require github.com/wagslane/go-tinytime v0.0.1
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	r "github.com/justhands13/reverse_int"
+	tinytime "github.com/wagslane/go-tinytime"
+)
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(tinytime.Now())
+	fmt.Println(r.ReverseInt(12938))
 }
