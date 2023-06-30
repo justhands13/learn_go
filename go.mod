d0361878a35132e964aa7b628919fc67c6717f8e
Shashank Pant <shashank11235@gmail.com>
2023-06-30 16:46:01 +0530

    greeting from another nested package

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.2
 require (
 	github.com/justhands13/reverse_int/v2 v2.0.0-20230605174952-977899bd1a4e
 	github.com/justhands13/reverse_string/v2 v2.0.0-20230630071557-0399c3d63e40
+	github.com/justhands13/web_scrapper/v2 v2.0.0-20230630111246-bc676b489bae
 	github.com/wagslane/go-tinytime v0.0.1
 )
--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 
 	r "github.com/justhands13/reverse_int/v2"
 	s "github.com/justhands13/reverse_string/v2"
+	g "github.com/justhands13/web_scrapper/v2/greeting"
 	tinytime "github.com/wagslane/go-tinytime"
 )
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(g.Greet())
 	fmt.Println(tinytime.Now())
 	fmt.Println(s.ReverseString("string desrever"))
 	fmt.Println(r.ReverseInt(12938))
