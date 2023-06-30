f55fbf6443971c38eca5028bc75f2b1a19fe9a6f
Shashank Pant <shashank11235@gmail.com>
2023-06-30 16:13:42 +0530

    added go.sum

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 
 	r "github.com/justhands13/reverse_int/v2"
+	s "github.com/justhands13/reverse_string/v2"
 	tinytime "github.com/wagslane/go-tinytime"
 )
 
 func main() {
 	fmt.Println("hello world")
 	fmt.Println(tinytime.Now())
+	fmt.Println(s.ReverseString("string desrever"))
 	fmt.Println(r.ReverseInt(12938))
 }
